9b07dc83d53d750cddb94fe3cd6c7545818e9f8b
boddak <kbh000111@naver.com>
2024-06-21 15:28:41 +0900

    로그인이름 옆 아이콘

--- src/main/webapp/resources/js/loginName.js
+++ src/main/webapp/resources/js/loginName.js
@@ -9,9 +9,9 @@ function loginName() {
         url: "member/getUserInfo",
         success: function(response) {
             // 사용자 정보가 성공적으로 로드되면 폼에 데이터 설정
-            if (response && response.user_name) {
+            if (response && response.user_nickname) {
                 $(".mainTopButton").prepend('<span class="userName">' +
-                    ''+ response.user_name + '님 ' + '환영합니다 |  </span>');
+                    ''+ response.user_nickname + '님 ' + '환영합니다 😊 |  </span>');
             } else {
                 console.error("사용자 이름을 가져오는 데 실패했습니다.");
             }
