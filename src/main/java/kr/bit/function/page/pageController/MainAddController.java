fe8e1d16f6acc5434f7b30c9f04d968fe483e424
sangwoo <whiz10@naver.com>
2024-07-03 04:10:19 +0900

    feat: 메인 오픈예정 페이지 구현

--- src/main/java/kr/bit/function/page/pageController/MainAddController.java
+++ src/main/java/kr/bit/function/page/pageController/MainAddController.java
@@ -2,7 +2,7 @@ package kr.bit.function.page.pageController;
 
 import kr.bit.function.board.boardEntity.PopupEntity;
 import kr.bit.function.board.boardEntity.FestivalEntity;
-import kr.bit.function.page.pageService.PopularAddService;
+import kr.bit.function.page.pageService.MainAddService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,22 +10,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
-public class PopularAddController {
+public class MainAddController {
 
     @Autowired
-    private PopularAddService popularAddService;
+    private MainAddService mainAddService;
 
     @GetMapping("/popularAddPopup")
     public String showPopularAddPopup(Model model) {
-        List<PopupEntity> popups = popularAddService.getAllPopups();
+        List<PopupEntity> popups = mainAddService.getAllPopups();
         model.addAttribute("popups", popups);
         return "page/searchResult/popularAddPopup";
     }
 
     @GetMapping("/popularAddFestival")
     public String showPopularAddFestival(Model model) {
-        List<FestivalEntity> festivals = popularAddService.getAllFestivals();
+        List<FestivalEntity> festivals = mainAddService.getAllFestivals();
         model.addAttribute("festivals", festivals);
         return "page/searchResult/popularAddFestival";
+    }
+
+    @GetMapping("/openAddPopup")
+    public String showAddPopup(Model model) {
+        List<PopupEntity> popups = mainAddService.getOpenPopups();
+        model.addAttribute("popups", popups);
+        return "page/searchResult/openAddPopup";
+    }
+
+    @GetMapping("/openAddFestival")
+    public String showAddFestival(Model model) {
+        List<FestivalEntity> festivals = mainAddService.getOpenFestivals();
+        model.addAttribute("festivals", festivals);
+        return "page/searchResult/openAddFestival";
     }
 }
